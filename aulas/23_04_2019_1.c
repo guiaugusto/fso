4d94de2a61e965abd798c7fc05170e644762fd22
Guilherme Augusto <guilherme.francais@gmail.com>
2019-04-30 10:19:47 -0300

    Cria solução inicial da comunicação entre processos por fila

--- aulas/23_04_2019_1.c
+++ aulas/23_04_2019_1.c
@@ -9,18 +9,36 @@
 mqd_t get_queue;
 mqd_t ret_queue;
 
-void request_ticket(){
+struct mq_attr attr;
+int ticket, msg = 1;
+int request_valid = 0;
+
+void open_queues(){
     if((get_queue = mq_open(GET_QUEUE, O_RDWR|O_CREAT, 0666, &attr)) < 0){
         perror("get mq_open");
         exit(1);
     }
+
+    if((ret_queue = mq_open(RET_QUEUE, O_RDWR|O_CREAT)) < 0){
+        perror("ret mq_open");
+        exit(1);
+    }
 }
 
-int receive_ticket(){
+int request_ticket(){
+    int send = mq_send (get_queue, (void *) &msg, sizeof(msg), 0);
+    if (send < 0){
+        perror("mq_send");
+        exit(1);
+    }else{
+        return 1;
+    }
+}
 
-    int ticket;
+int receive_ticket(int ticket){
 
-    if(mq_receive(ret_queue, (void*) &ticket, sizeof(ticket), 0) < 0){
+    int receive = mq_receive(ret_queue, (void*) &ticket, sizeof(ticket), 0);
+    if(receive < 0){
         perror("mq_receive:");
         exit(1);
     }
@@ -37,25 +55,22 @@ void give_back_ticket(int ticket){
 
 int main(){
 
-    struct mq_attr attr;
-    int ticket, msg;
-
     attr.mq_maxmsg = 10;
-    attr.mq_msgsize = sizeof(msg);
+    attr.mq_msgsize = sizeof(ticket)*2;
     attr.mq_flags = 0;
 
-
-
+    open_queues();
 
     for(;;){
 
-        if (mq_send (get_queue, (void *) &msg, sizeof(msg), 0) < 0){
-            perror("mq_send");
-            exit(1);
-        }
+        msg = 1;
 
-        printf("Esperando envio de ticket\n");
+        printf("Solicitando ticket!\n");
 
-        ticket = receive_ticket();
+        request_ticket();
+        printf("Ticket solicitado!\n");
+
+        receive_ticket(ticket);
+        printf("Ticket recebido: %d\n", ticket);
     }
 }
--- aulas/23_04_2019_2.c
+++ aulas/23_04_2019_2.c
@@ -2,26 +2,82 @@
 #include <stdlib.h>
 #include <mqueue.h>
 #include <unistd.h>
+#include <string.h>
 
-#define QUEUE "/my_queue"
+#define GET_QUEUE "/get_queue"
+#define RET_QUEUE "/ret_queue"
+#define MAX_TICKETS 100
 
-int main(int argc, char *argv[]){
-    mqd_t queue;
-    int msg;
+mqd_t get_queue;
+mqd_t ret_queue;
+int ticket = 0, msg;
+int tickets[MAX_TICKETS];
+struct mq_attr attr;
 
-    if((queue = mq_open(QUEUE, O_RDWR)) < 0){
-        perror("mq_open");
+void open_queues(){
+    if((get_queue = mq_open(GET_QUEUE, O_RDWR)) < 0){
+        perror("get mq_open");
         exit(1);
     }
 
-    for(;;){
-        msg = random() % 100;
+    if((ret_queue = mq_open(RET_QUEUE, O_RDWR|O_CREAT, 0666, &attr)) < 0){
+        perror("ret mq_open");
+        exit(1);
+    }
+}
 
-        if (mq_send (queue, (void *) &msg, sizeof(msg), 0) < 0){
-            perror("mq_send");
-            exit(1);
+int get_ticket(){
+    int i;
+    int t;
+    for(i = 0; i < MAX_TICKETS; i++){
+        if(tickets[i] == -1){
+            t = i;
+            break;
         }
-        printf("Sent message with value %d\n", msg);
+    }
+
+    if(i == MAX_TICKETS-1){
+        t = -1;
+    }
+
+    return t;
+}
+
+void get_request(){
+    int receive = mq_receive(get_queue, (void*) &msg, sizeof(msg), 0);
+    if(receive < 0){
+        perror("mq_receive:");
+        exit(1);
+    }
+}
+
+void send_ticket(){
+    if (mq_send (ret_queue, (void *) &ticket, sizeof(ticket), 0) < 0){
+        perror("mq_send");
+        exit(1);
+    }
+}
+
+int main(int argc, char *argv[]){
+
+    memset(tickets, -1, sizeof(tickets));
+
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = sizeof(ticket);
+    attr.mq_flags = 0;
+
+    open_queues();
+
+    for(;;){
+        get_request();
+        printf("Um pedido foi solicitado!\n");
+
+        ticket = get_ticket();
+        printf("Ticket %d selecionado!\n", ticket);
+
+        send_ticket(ticket);
+        printf("Ticket %d foi enviado!\n", ticket);
+
         sleep(1);
     }
 }
